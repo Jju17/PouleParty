20763a6b3131bbb3fd7608a62f068d3f82a7c531
julien.rahier <julien.rahier@persgroep.net>
2024-03-14 23:04:38 +0100

    Initial Commit

---
+++ ChickenRush/ChickenRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenRushApp.swift
+//  ChickenRush
+//
+//  Created by Julien Rahier on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChickenRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
